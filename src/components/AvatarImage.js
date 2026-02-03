c399482d5d2fbf6b29037dff7b2e80021bdd379a
Ex <extrepa@Extrepas-4k-Baby.local>
2026-02-03 11:16:16 -0800

    Update forum application

--- src/components/AvatarImage.js
+++ src/components/AvatarImage.js
@@ -39,7 +39,6 @@ export default function AvatarImage({
     <img
       src={resolvedSrc}
       alt={alt}
-      loading={loading}
       decoding={decoding}
       draggable={draggable}
       className={className}
